493c0764119f61710fe286f1e971fdf35655bac6
Patrick Pale <patrick.pale@atomicobject.com>
2020-02-03 14:22:14 -0500

    Added tests

--- src/test/java/com/example/restservice/WebMockTest.java
+++ src/test/java/com/example/restservice/WebMockTest.java
@@ -1,31 +1,34 @@
 package com.example.restservice;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
-import org.junit.jupiter.api.Test;
-
 import static org.hamcrest.Matchers.containsString;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
-@AutoConfigureMockMvc
-class RestServiceApplicationTests {
+@WebMvcTest(GreetingController.class)
+public class WebMockTest {
 
   @Autowired
   private MockMvc mockMvc;
 
-	@Test
-	public void greetingShouldReturnDefaultMessage() throws Exception {
+  @MockBean
+  private GreetingService service;
+
+  @Test
+  public void greetingShouldReturnMessageFromService() throws Exception {
+    Greeting greeting = new Greeting(1, "Hello, World!");
+    when(service.greet("World")).thenReturn(greeting);
     this.mockMvc.perform(get("/greeting"))
       .andDo(print())
       .andExpect(status().isOk())
       .andExpect(content().string(containsString("Hello, World!")));
 	}
-
 }
